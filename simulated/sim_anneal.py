27d3c6e5e5aaec61374077e90245fa45b5ac14e8
aerorohit <rohittembhare1@gmail.com>
2021-03-10 20:58:50 +0530

    initial code for wake and sim annealing

--- simulated/sim_anneal.py
+++ simulated/sim_anneal.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def perturb_state(x, per_max, n):
-    per = per_max*(2*np.random.random(n)-1)
-    x = x+per
+def perturb_state(x, per_max, k):
+    per = per_max*(2*np.random.rand()-1)
+    x[k] = x[k] + per
+    return(x)
 
 def annealing(x0, fn_obj, Tmax, Tmin, alpha, Markov_no, per_max):
     x_best = copy(x0)
@@ -18,18 +19,21 @@ def annealing(x0, fn_obj, Tmax, Tmin, alpha, Markov_no, per_max):
 
     while T>Tmin:
         for i in range(Markov_no):
-            xi = perturb_state(x, per_max, n)
-            cost_i = fn_obj(xi)
-            dcost = cost - cost_i
+            for k in range(n):
+                xi = perturb_state(x, per_max, k)
+                cost_i = fn_obj(xi)
+                dcost = cost - cost_i
 
-            if dcost<0:
-                x = xi.copy()
-                if cost_best > cost_i:
-                    cost_best = cost_i
-                    x_best = x.copy()
+                if dcost<0:
+                    x = xi.copy()
+                    cost = cost_i
 
-            elif np.exp(-dcost/T) > np.random.rand():
-                x = xi.copy()
+                    if cost< cost_best:
+                        cost_best = cost
+                        x_best = x.copy()
+
+                elif np.exp(-dcost/T) > np.random.rand():
+                    x = xi.copy()
 
         T = alpha*T
     return(x_best, cost_best)
