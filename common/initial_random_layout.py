e224f90bc857254ca910665a3b94d6f7e1e7ebae
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2021-03-11 22:29:32 +0530

    some progress in genetic

--- common/initial_random_layout.py
+++ common/initial_random_layout.py
@@ -10,7 +10,9 @@ class Layout:
 		layout[1::2] = Y
 		self.layout = layout
 
-def random_layout(N, A, B, grid_size):
+def random_layout(N, x_bound, y_bound, grid_size):
+	A = int( (x_bound[1] - x_bound[0])/grid_size )
+	B = int( (y_bound[1] - y_bound[0])/grid_size )
 	random_grids = random.sample(range(A*B), N)
 	X = np.zeros(N)
 	Y = np.zeros(N)
@@ -22,4 +24,3 @@ def random_layout(N, A, B, grid_size):
 	
 	return Layout(X, Y, N)
 
-	
--- genetic/genetic.py
+++ genetic/genetic.py
@@ -1,49 +1,51 @@
+# not working
+import random
 import numpy as np
+from ..common import initial_random_layout
+from ..common import objective_function
 
 class Population:
-	def __init__(self, n_pop, x_bound, y_bound, N):
+	def __init__(self, n_pop, x_bound, y_bound, N, crossover_prob, grid_size):
 		self.n_pop = n_pop
 		self.x_bound = x_bound
 		self.y_bound = y_bound
-		self.pop_matrix = np.zeros(n_pop, 2*N)
-		self.elite_var = np.floor(0.1*n_pop)
+		self.pop_matrix = np.zeros((n_pop, 2*N+1))
+		self.N = N
+		self.crossover_pop = np.floor(crossover_prob*n_pop)
+		self.grid_size = grid_size
 
 	def initialise_random(self):
-		pass
+		for i in range(self.n_pop):
+			self.pop_matrix[i, 1:] = initial_random_layout.random_layout(self.N, self.x_bound, self.y_bound, self.grid_size).layout
 
-	def fitness_pop(self):
-		pass
+	# make it compatible
+	def fitness_pop(self, pop_matrix):
+		for i in range(self.n_pop):
+			pop_matrix[i, 0] = objective_function(pop_matrix[i, 1:])
+		
+		self.pop_matrix = self.pop_matrix[np.argsort(self.pop_matrix[:, 0])]
 
-	def elite(self, oldPop):
-		self.pop_matrix[:]
-
-	# only for new layouts
+	# make beta different for all variables
+	def crossover(self):
+		new_pop_matrix = np.zeros((self.n_pop, 2*self.N+1))
+		for i in range(self.n_pop):
+			r = random.sample(range(self.crossover_pop), 3)
+			beta = r[0]/self.crossover_pop
+			new_pop_matrix[i, 1:] = beta*self.pop_matrix[r[1], 1:] + (1-beta)*self.pop_matrix[r[2], 1:]
+		
+		self.new_pop_matrix = new_pop_matrix
+	
 	def mutation(self):
 		pass
-
-	def crossover(self):
-		pass
-
-class Layout:
-	def __init__(self, X, Y, N):
-		self.N = N
-		layout = np.zeros(2*N)
-		layout[::2] = X
-		layout[1::2] = Y
-		self.layout = layout
-
-	def calc_cost(self):
-		self.cost = function_call(self.layout)
-
+		
 
 oldPop = Population(10, [0, 50], [0, 50], 23)
 oldPop.initialise_random()
 oldPop.fitness_pop()
-for i in range(10):
+for i in range(20):
 	newPop = Population(10, [0, 50], [0, 50], 23)
-	newPop.elite()
 	newPop.crossover()
 	newPop.mutation()
-	newPop.fitness_pop()
+	newPop.fitness_pop(newPop.new_pop_matrix)
 	print("{}th generation - best cost: {}".format(i, newPop.pop_matrix[0].cost))
 	oldPop = newPop
