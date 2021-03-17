eeb2678d19b46424a2634dc6ea6b3deb436362f6
amalss18 <amalssebastian@gmail.com>
2021-03-17 23:18:05 +0530

    updated visualisation with circles

--- common/wake_visualization.py
+++ common/wake_visualization.py
@@ -8,9 +8,9 @@ from matplotlib.patches import Polygon
 def get_turbine_lines(x, y, wind):
     normal = np.array([-wind[1]/(wind[0]+np.finfo(float).eps), 1])
     normal = normal/np.linalg.norm(normal)
-    R = 41
+    R = 20
 
-    Z_H = 80  # Hub height of rotor in m
+    Z_H = 60  # Hub height of rotor in m
     Z_0 = 0.3  # Hub height of rotor in m
     alpha = 0.5 / (np.log(Z_H / Z_0))
 
@@ -22,9 +22,9 @@ def get_turbine_lines(x, y, wind):
         pos1 = turbine_pos + np.array([0, R])
         pos2 = turbine_pos - np.array([0, R])
 
-        plt.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]])
+        plt.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], color='red')
 
-        dist = 150*R
+        dist = 12.2*np.sqrt(2)*R
         rl = alpha*dist + R
 
         pos4 = turbine_pos + np.array([dist, rl])
@@ -35,6 +35,32 @@ def get_turbine_lines(x, y, wind):
 
         plt.fill(x_pos, y_pos, facecolor='lightsalmon', edgecolor='orangered', alpha=0.25)
 
+def get_turbine_circles(x, y, wind):
+    Z_H = 60  # Hub height of rotor in m
+    Z_0 = 0.3  # Hub height of rotor in m
+    alpha = 0.5 / (np.log(Z_H / Z_0))
+    diameter = 40
+    R = 5 * diameter
+
+    x_lines, y_lines = [], []
+    for i in range(len(x)):
+        turbine_pos = np.array([x[i], y[i]])
+
+        x_range = np.linspace(x[i] - 0.99*R, x[i] + 0.99*R, 100)
+        y_range = y[i] + np.sqrt((R ** 2) - (x_range - x[i])**2)
+        y_range_2 = y[i] - np.sqrt((R ** 2) - (x_range - x[i])**2)
+
+        y_range = np.concatenate((y_range, y_range_2[::-1]))
+        x_range = np.concatenate((x_range, x_range[::-1]))
+
+
+        plt.fill(x_range, y_range, facecolor='lightsteelblue', edgecolor='royalblue', alpha=0.25)
+        # plt.fill(x_range, y_range_2, facecolor='lightsteelblue', edgecolor='royalblue', alpha=0.25)
+
+
+        # plt.fill(x_pos, y_pos, facecolor='lightsalmon', edgecolor='orangered', alpha=0.25)
+
+
 
 def get_wake_plots(x, y, wind_dir=[12, 0]):
     """
@@ -43,9 +69,10 @@ def get_wake_plots(x, y, wind_dir=[12, 0]):
     """
 
     get_turbine_lines(x, y, wind_dir)
-    plt.scatter(x, y)
-    plt.xlim(0, 4000)
-    plt.ylim(0, 3500)
+    get_turbine_circles(x, y, wind_dir)
+    plt.scatter(x, y, color='black')
+    plt.xlim(0, 2000)
+    plt.ylim(0, 2000)
     plt.show()
     # plt.plot(x_lines, y_lines)
 
