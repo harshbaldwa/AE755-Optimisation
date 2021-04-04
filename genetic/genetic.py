c74d5dfd35a713163f4608ca5fb8ce73d336c37a
aerorohit <rohittembhare1@gmail.com>
2021-04-04 12:53:28 +0530

    modified wake visualisation

--- genetic/genetic.py
+++ genetic/genetic.py
@@ -1,5 +1,5 @@
 # cost functions
-from ..common import mosetti_cost as cost
+from ..common import cost as cost
 from ..common.windrose import read_windrose
 # visualization
 from ..common.wake_visualization import get_wake_plots
@@ -48,14 +48,14 @@ def mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range):
 
 
 # turbine data
-diameter = 40
-height = 60
+diameter = 82
+height = 80
 z_0 = 0.3
 
 # turbines and farm
 N = 26
 n_var = 2*N
-bounds = np.array([[0, 2000], [0, 2000]])
+bounds = np.array([[0, 4000], [0, 3500]])
 b_range = np.array([bounds[0, 1] - bounds[0, 0], bounds[1, 1] - bounds[1, 0]])
 windspeed_array, theta_array, wind_prob = read_windrose()
 
@@ -89,12 +89,10 @@ try :
 
 except KeyboardInterrupt:
     print(new_pop[0, 1:])
-    # get_wake_plots(new_pop[0, 1::2], new_pop[0, 2::2], bounds, diameter, height, z_0, wind_velocity, "Genetic")
-    get_wake_plots(new_pop[0, 1::2], new_pop[0, 2::2], bounds, "Genetic")
+    get_wake_plots(new_pop[0, 1::2], new_pop[0, 2::2], bounds, diameter, height, z_0, windspeed_array, theta_array, wind_prob, "Genetic")
 
 
 print(new_pop[0, 1:])
-# get_wake_plots(new_pop[0, 1::2], new_pop[0, 2::2], bounds, diameter, height, z_0, wind_velocity, "Genetic")
-get_wake_plots(new_pop[0, 1::2], new_pop[0, 2::2], bounds, "Genetic")
+get_wake_plots(new_pop[0, 1::2], new_pop[0, 2::2], bounds, diameter, height, z_0, windspeed_array, theta_array, wind_prob, "Genetic")
 
 
