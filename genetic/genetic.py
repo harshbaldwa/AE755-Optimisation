d5d420607fd0d4c34483b1ba7e955191be46ebdf
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2021-04-13 12:09:47 +0530

    some new results

--- genetic/genetic.py
+++ genetic/genetic.py
@@ -1,4 +1,5 @@
 # cost functions
+from numba.np.ufunc import parallel
 from ..common import cost as cost
 from ..common.windrose import read_windrose
 
@@ -8,6 +9,7 @@ from ..common.wake_visualization import get_wake_plots
 # other requirements
 import numpy as np
 from progress.bar import IncrementalBar
+from time import time
 
 
 def init_random(pop, n_pop, bounds, N):
@@ -36,7 +38,6 @@ def init_fitness(
         )
 
     return pop[np.argsort(pop[:, 0])]
-
 
 def fitness(
     pop,
@@ -74,16 +75,21 @@ def cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size):
     parents_idx.sort(axis=1)
     beta = 0.5 - np.random.random((cross_num, n_var))
 
-    for i in range(0, cross_num):
-        parent1 = old_pop[parents_idx[i, 0], 1:]
-        parent2 = old_pop[parents_idx[i, 1], 1:]
-        new_pop[elit_num + i, 1:] = beta[i] * parent1 + (1 - beta[i]) * parent2
+    parents1 = old_pop[parents_idx[:, 0], 1:]
+    parents2 = old_pop[parents_idx[:, 1], 1:]
+
+    new_pop[elit_num:elit_num+cross_num, 1:] = beta*parents1 + (1-beta)*parents2
+
+    # for i in range(0, cross_num):
+    #     parent1 = old_pop[parents_idx[i, 0], 1:]
+    #     parent2 = old_pop[parents_idx[i, 1], 1:]
+    #     new_pop[elit_num + i, 1:] = beta[i] * parent1 + (1 - beta[i]) * parent2
 
 
-def mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range):
+def mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range, range_par):
     parents_idx = np.random.randint(0, n_pop, mutat_num)
     gene_idx = np.random.randint(1, n_var + 1, (mutat_num, mutat_gene))
-    gamma = 0.3 - 0.6 * np.random.random((mutat_num, mutat_gene))
+    gamma = range_par - 2*range_par * np.random.random((mutat_num, mutat_gene))
 
     for i in range(0, mutat_num):
         new_pop[-(i + 1), 1:] = old_pop[parents_idx[i], 1:]
@@ -93,7 +99,7 @@ def mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range):
 # turbine data
 diameter = 82
 height = 80
-z_0 = 0.3
+z_0 = 0.5
 
 # turbines and farm
 N = 33
@@ -107,18 +113,18 @@ wind_prob = np.array([1])
 
 
 # optimizer variables
-n_pop = 150
-elit_num = 20
+n_pop = 200
+elit_num = 24
 cross_frac = 0.8
 cross_num = int(cross_frac * (n_pop - elit_num))
 tour_size = 5
-mutat_gene = 1
+mutat_gene = 2
+range_par = 0.5
 mutat_num = n_pop - elit_num - cross_num
 old_pop = np.zeros((n_pop, n_var + 1))
 new_pop = np.zeros((n_pop, n_var + 1))
 
-generations = 400
-
+generations = 100
 
 # start algorithm
 init_random(old_pop, n_pop, bounds, N)
@@ -134,56 +140,72 @@ old_pop = init_fitness(
     wind_prob,
 )
 
+cross_time = 0
+mutate_time = 0
+fitness_time = 0
+
+a = time()
+
 try:
-    with IncrementalBar(
-        "Genetic",
-        max=generations,
-        suffix="%(percent).1f%% time_elapsed:[%(elapsed)ds] estimated_time:[%(eta)ds]",
-    ) as bar:
-        for gen in range(generations):
-            elite(new_pop, old_pop, elit_num)
-            cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size)
-            mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range)
-            new_pop = fitness(
-                new_pop,
-                n_pop,
-                elit_num,
-                bounds,
-                diameter,
-                height,
-                z_0,
-                windspeed_array,
-                theta_array,
-                wind_prob,
-            )
-            # print(gen, " - ", new_pop[0:3, 0])
-            bar.next()
-            old_pop = new_pop
+    # with IncrementalBar(
+    #     "Genetic",
+    #     max=generations,
+    #     suffix="%(percent).1f%% time_elapsed:[%(elapsed)ds] estimated_time:[%(eta)ds]",
+    # ) as bar:
+    for gen in range(generations):
+        elite(new_pop, old_pop, elit_num)
+        d=time()
+        cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size)
+        e=time()
+        mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range, range_par)
+        f=time()
+        new_pop = fitness(
+            new_pop,
+            n_pop,
+            elit_num,
+            bounds,
+            diameter,
+            height,
+            z_0,
+            windspeed_array,
+            theta_array,
+            wind_prob,
+        )
+        g=time()
+        # print("{} - {}, {}, {}".format(gen, new_pop[0, 0], new_pop[1, 0], new_pop[2, 0]))
+        # bar.next()
+        old_pop = new_pop
+        cross_time += e-d
+        mutate_time += f-e
+        fitness_time += g-f
 
 except KeyboardInterrupt:
     print("Getting the values from last population...\n")
 
+b = time()
 
-algo_data = [
-    "Genetic",
-    "n_pop: {}\nelit_num: {}\ncross: {}\nmutate_gene: {}\ngen: {}".format(
-        n_pop, elit_num, cross_frac, mutat_gene, generations
-    ),
-    "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: ${:.2f}M".format(
-        N, diameter, height, 'tejas', -new_pop[0, 0] / 1e6
-    ),
-    "genetic_{}".format(N),
-]
-print("Profit - ${:.2f}M".format(-new_pop[0, 0] / 1e6))
-get_wake_plots(
-    new_pop[0, 1::2],
-    new_pop[0, 2::2],
-    bounds,
-    diameter,
-    height,
-    z_0,
-    windspeed_array,
-    theta_array,
-    wind_prob,
-    algo_data
-)
+print(cross_time, mutate_time, fitness_time)
+
+# algo_data = [
+#     "Genetic",
+#     "n_pop: {}\nelit_num: {}\ncross: {}\nmutate_gene: {}\nrange_par: {}\ngen: {}".format(
+#         n_pop, elit_num, cross_frac, mutat_gene, range_par, generations
+#     ),
+#     "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: ${:.3f}M\ntime: {:.3f}s".format(
+#         N, diameter, height, 'tejas', -new_pop[0, 0] / 1e6, b-a
+#     ),
+#     "genetic_{}".format(N),
+# ]
+# print("Profit - ${:.3f}M".format(-new_pop[0, 0] / 1e6))
+# get_wake_plots(
+#     new_pop[0, 1::2],
+#     new_pop[0, 2::2],
+#     bounds,
+#     diameter,
+#     height,
+#     z_0,
+#     windspeed_array,
+#     theta_array,
+#     wind_prob,
+#     algo_data
+# )
