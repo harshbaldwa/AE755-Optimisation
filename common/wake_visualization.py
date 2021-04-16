fb942fa110dd04e3017610df18efeb1d4812562c
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2021-04-16 16:33:23 +0530

    interactive plots

--- common/wake_visualization.py
+++ common/wake_visualization.py
@@ -21,7 +21,7 @@ def get_turbine_lines(x, y, diameter, height, z_0, wind):
 
         plt.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], color='orange')
 
-        dist = 12.2*R
+        dist = 100*R
         rl = alpha*dist + R
 
         pos4 = turbine_pos + np.array([dist, rl])
@@ -33,7 +33,7 @@ def get_turbine_lines(x, y, diameter, height, z_0, wind):
         plt.fill(x_pos, y_pos, facecolor='lightsalmon', edgecolor='orangered', alpha=0.25)
 
 def get_turbine_circles(x, y, diameter):
-    R = 2.5 * diameter
+    R = 5 * diameter
 
     for i in range(len(x)):
 
@@ -46,6 +46,25 @@ def get_turbine_circles(x, y, diameter):
 
 
         plt.fill(x_range, y_range, facecolor='lightsteelblue', edgecolor='royalblue', alpha=0.2)
+
+
+def get_wake_plots_interactive(x, y, bounds, diameter, height, z_0, windspeed_array, theta_array, wind_prob, algo_data):
+    """
+    x, y: 1D arays are turbines coordinates
+    wind: 1x2 array of x and y components of the wind.
+    """
+    get_turbine_lines(x, y, diameter, height, z_0, [windspeed_array[0], 0])
+    get_turbine_circles(x, y, diameter)
+    plt.scatter(x, y, color='gray')
+    b1 = bounds[0][0]
+    b2 = bounds[0][1]
+    b3 = bounds[1][0]
+    b4 = bounds[1][1]
+    plt.plot([b1, b1, b2, b2, b1], [b3, b4, b4, b3, b3], color='white', linestyle="-", linewidth=0.5)
+    b_range = 0.2*np.array([bounds[0, 1] - bounds[0, 0], bounds[1, 1] - bounds[1, 0]])
+    plt.xlim(bounds[0, 0] - b_range[0], bounds[0, 1] + b_range[0])
+    plt.ylim(bounds[1, 0] - b_range[1], bounds[1, 1] + b_range[1])
+    plt.draw()
 
 
 def get_wake_plots(x, y, bounds, diameter, height, z_0, windspeed_array, theta_array, wind_prob, algo_data):
@@ -74,5 +93,3 @@ def get_wake_plots(x, y, bounds, diameter, height, z_0, windspeed_array, theta_a
         plt.savefig('./opti/results/{}_{}.png'.format(algo_data[3], int(time.time())))
     else:
         plt.savefig('./opti/results/{}.png'.format(algo_data[3]))
-    
-
--- genetic/genetic.py
+++ genetic/genetic.py
@@ -1,5 +1,4 @@
 # cost functions
-from numba.np.ufunc import parallel
 from ..common import cost as cost
 from ..common.windrose import read_windrose
 
@@ -13,87 +12,87 @@ from time import time
 
 
 def init_random(pop, n_pop, bounds, N):
-    for i in range(n_pop):
-        pop[i, 1::2] = bounds[0, 0] + np.random.random(N) * (
-            bounds[0, 1] - bounds[0, 0]
-        )
-        pop[i, 2::2] = bounds[1, 0] + np.random.random(N) * (
-            bounds[1, 1] - bounds[1, 0]
-        )
+	for i in range(n_pop):
+		pop[i, 1::2] = bounds[0, 0] + np.random.random(N) * (
+			bounds[0, 1] - bounds[0, 0]
+		)
+		pop[i, 2::2] = bounds[1, 0] + np.random.random(N) * (
+			bounds[1, 1] - bounds[1, 0]
+		)
 
 
 def init_fitness(
-    pop, n_pop, bounds, diameter, height, z_0, windspeed_array, theta_array, wind_prob
+	pop, n_pop, bounds, diameter, height, z_0, windspeed_array, theta_array, wind_prob
 ):
-    for i in range(n_pop):
-        pop[i, 0] = cost.objective(
-            pop[i, 1:],
-            bounds,
-            diameter,
-            height,
-            z_0,
-            windspeed_array,
-            theta_array,
-            wind_prob,
-        )
+	for i in range(n_pop):
+		pop[i, 0] = cost.objective(
+			pop[i, 1:],
+			bounds,
+			diameter,
+			height,
+			z_0,
+			windspeed_array,
+			theta_array,
+			wind_prob,
+		)
 
-    return pop[np.argsort(pop[:, 0])]
+	return pop[np.argsort(pop[:, 0])]
 
 def fitness(
-    pop,
-    n_pop,
-    elit_num,
-    bounds,
-    diameter,
-    height,
-    z_0,
-    windspeed_array,
-    theta_array,
-    wind_prob,
+	pop,
+	n_pop,
+	elit_num,
+	bounds,
+	diameter,
+	height,
+	z_0,
+	windspeed_array,
+	theta_array,
+	wind_prob,
 ):
-    for i in range(elit_num, n_pop):
-        pop[i, 0] = cost.objective(
-            pop[i, 1:],
-            bounds,
-            diameter,
-            height,
-            z_0,
-            windspeed_array,
-            theta_array,
-            wind_prob,
-        )
+	for i in range(elit_num, n_pop):
+		pop[i, 0] = cost.objective(
+			pop[i, 1:],
+			bounds,
+			diameter,
+			height,
+			z_0,
+			windspeed_array,
+			theta_array,
+			wind_prob,
+		)
 
-    return pop[np.argsort(pop[:, 0])]
+	return pop[np.argsort(pop[:, 0])]
 
 
 def elite(new_pop, old_pop, elit_num):
-    new_pop[:elit_num] = old_pop[:elit_num]
+	new_pop[:elit_num] = old_pop[:elit_num]
 
 
 def cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size):
-    parents_idx = np.random.randint(0, n_pop, (cross_num, tour_size))
-    parents_idx.sort(axis=1)
-    beta = 0.5 - np.random.random((cross_num, n_var))
+	parents_idx = np.random.randint(0, n_pop, (cross_num, tour_size))
+	parents_idx.sort(axis=1)
+	beta = 0.5 - np.random.random((cross_num, n_var))
 
-    parents1 = old_pop[parents_idx[:, 0], 1:]
-    parents2 = old_pop[parents_idx[:, 1], 1:]
+	parents1 = old_pop[parents_idx[:, 0], 1:]
+	parents2 = old_pop[parents_idx[:, 1], 1:]
 
-    new_pop[elit_num:elit_num+cross_num, 1:] = beta*parents1 + (1-beta)*parents2
+	new_pop[elit_num:elit_num+cross_num, 1:] = beta*parents1 + (1-beta)*parents2
 
-    # for i in range(0, cross_num):
-    #     parent1 = old_pop[parents_idx[i, 0], 1:]
-    #     parent2 = old_pop[parents_idx[i, 1], 1:]
-    #     new_pop[elit_num + i, 1:] = beta[i] * parent1 + (1 - beta[i]) * parent2
+	# for i in range(0, cross_num):
+	#	  parent1 = old_pop[parents_idx[i, 0], 1:]
+	#	  parent2 = old_pop[parents_idx[i, 1], 1:]
+	#	  new_pop[elit_num + i, 1:] = beta[i] * parent1 + (1 - beta[i]) * parent2
 
 
 def mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range, range_par):
-    parents_idx = np.random.randint(0, n_pop, mutat_num)
-    gene_idx = np.random.randint(1, n_var + 1, (mutat_num, mutat_gene))
-    gamma = range_par - 2*range_par * np.random.random((mutat_num, mutat_gene))
+	parents_idx = np.random.randint(0, n_pop, mutat_num)
+	gene_idx = np.random.randint(1, n_var + 1, (mutat_num, mutat_gene))
+	gamma = range_par - 2*range_par * np.random.random((mutat_num, mutat_gene))
 
-    for i in range(0, mutat_num):
-        new_pop[-(i + 1), 1:] = old_pop[parents_idx[i], 1:]
-        new_pop[-(i + 1), gene_idx[i]] += gamma[i] * b_range[gene_idx[i] % 2]
+	for i in range(0, mutat_num):
+		new_pop[-(i + 1), 1:] = old_pop[parents_idx[i], 1:]
+		new_pop[-(i + 1), gene_idx[i]] += gamma[i] * b_range[gene_idx[i] % 2]
 
 
 # turbine data
@@ -102,7 +101,7 @@ height = 80
 z_0 = 0.5
 
 # turbines and farm
-N = 33
+N = 15 
 n_var = 2 * N
 bounds = np.array([[0, 4000], [0, 3500]])
 b_range = np.array([bounds[0, 1] - bounds[0, 0], bounds[1, 1] - bounds[1, 0]])
@@ -124,88 +123,98 @@ mutat_num = n_pop - elit_num - cross_num
 old_pop = np.zeros((n_pop, n_var + 1))
 new_pop = np.zeros((n_pop, n_var + 1))
 
-generations = 100
+generations = 2000
 
 # start algorithm
 init_random(old_pop, n_pop, bounds, N)
 old_pop = init_fitness(
-    old_pop,
-    n_pop,
-    bounds,
-    diameter,
-    height,
-    z_0,
-    windspeed_array,
-    theta_array,
-    wind_prob,
+	old_pop,
+	n_pop,
+	bounds,
+	diameter,
+	height,
+	z_0,
+	windspeed_array,
+	theta_array,
+	wind_prob,
 )
-
-cross_time = 0
-mutate_time = 0
-fitness_time = 0
 
 a = time()
 
 try:
-    # with IncrementalBar(
-    #     "Genetic",
-    #     max=generations,
-    #     suffix="%(percent).1f%% time_elapsed:[%(elapsed)ds] estimated_time:[%(eta)ds]",
-    # ) as bar:
-    for gen in range(generations):
-        elite(new_pop, old_pop, elit_num)
-        d=time()
-        cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size)
-        e=time()
-        mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range, range_par)
-        f=time()
-        new_pop = fitness(
-            new_pop,
-            n_pop,
-            elit_num,
-            bounds,
-            diameter,
-            height,
-            z_0,
-            windspeed_array,
-            theta_array,
-            wind_prob,
-        )
-        g=time()
-        # print("{} - {}, {}, {}".format(gen, new_pop[0, 0], new_pop[1, 0], new_pop[2, 0]))
-        # bar.next()
-        old_pop = new_pop
-        cross_time += e-d
-        mutate_time += f-e
-        fitness_time += g-f
+	with IncrementalBar(
+		"Genetic",
+		max=generations,
+		suffix="%(percent).1f%% time_elapsed:[%(elapsed)ds] estimated_time:[%(eta)ds]",
+	) as bar:
+		for gen in range(generations):
+			elite(new_pop, old_pop, elit_num)
+			cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size)
+			mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range, range_par)
+			new_pop = fitness(
+				new_pop,
+				n_pop,
+				elit_num,
+				bounds,
+				diameter,
+				height,
+				z_0,
+				windspeed_array,
+				theta_array,
+				wind_prob,
+			)
+			# print("{} - {}, {}, {}".format(gen, new_pop[0, 0], new_pop[1, 0], new_pop[2, 0]))
+			bar.next()
+			# if gen%25 == 0:
+			#     algo_data = [
+			# 	    "Genetic",
+			# 	    "n_pop: {}\nelit_num: {}\ncross: {}\nmutate_gene: {}\nrange_par: {}\ngen: {}".format(
+			# 		    n_pop, elit_num, cross_frac, mutat_gene, range_par, generations
+			# 	    ),
+			# 	    "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: ${:.3f}M\n gen: {}".format(
+			# 		    N, diameter, height, 'tejas', -new_pop[0, 0] / 1e6, gen
+			# 	    ),
+			# 	    "genetic_{}_{}".format(N, gen),
+			#     ]
+			#     get_wake_plots(
+			# 	    new_pop[0, 1::2],
+			# 	    new_pop[0, 2::2],
+			# 	    bounds,
+			# 	    diameter,
+			# 	    height,
+			# 	    z_0,
+			# 	    windspeed_array,
+			# 	    theta_array,
+			# 	    wind_prob,
+			# 	    algo_data
+			#     )
+			old_pop = new_pop
 
 except KeyboardInterrupt:
-    print("Getting the values from last population...\n")
+	print("Getting the values from last population...\n")
 
 b = time()
 
-print(cross_time, mutate_time, fitness_time)
-
-# algo_data = [
-#     "Genetic",
-#     "n_pop: {}\nelit_num: {}\ncross: {}\nmutate_gene: {}\nrange_par: {}\ngen: {}".format(
-#         n_pop, elit_num, cross_frac, mutat_gene, range_par, generations
-#     ),
-#     "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: ${:.3f}M\ntime: {:.3f}s".format(
-#         N, diameter, height, 'tejas', -new_pop[0, 0] / 1e6, b-a
-#     ),
-#     "genetic_{}".format(N),
-# ]
-# print("Profit - ${:.3f}M".format(-new_pop[0, 0] / 1e6))
-# get_wake_plots(
-#     new_pop[0, 1::2],
-#     new_pop[0, 2::2],
-#     bounds,
-#     diameter,
-#     height,
-#     z_0,
-#     windspeed_array,
-#     theta_array,
-#     wind_prob,
-#     algo_data
-# )
+algo_data = [
+         "Genetic",
+         "n_pop: {}\nelit_num: {}\ncross: {}\nmutate_gene: {}\nrange_par: {}\ngen: {}".format(
+       	  n_pop, elit_num, cross_frac, mutat_gene, range_par, generations
+         ),
+         "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: ${:.3f}M\ntime: {:.3f}s".format(
+       	  N, diameter, height, 'tejas', -new_pop[0, 0] / 1e6, b-a
+         ),
+         "genetic_{}_{}".format(N, generations),
+]
+print("Profit - ${:.3f}M".format(-new_pop[0, 0] / 1e6))
+get_wake_plots(
+         new_pop[0, 1::2],
+         new_pop[0, 2::2],
+         bounds,
+         diameter,
+         height,
+         z_0,
+         windspeed_array,
+         theta_array,
+         wind_prob,
+         algo_data
+)
