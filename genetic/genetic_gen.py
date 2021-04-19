aa2f3f585a19fd0722734d0f9c7deb95d548aa4d
Harshvardhan Baldwa <harshbaldwa@gmail.com>
2021-04-20 02:42:44 +0530

    working genetic

--- genetic/genetic_gen.py
+++ genetic/genetic_gen.py
@@ -11,6 +11,27 @@ import numpy as np
 from tqdm import tqdm as bar
 from time import time
 
+# turbine data
+diameter = 82
+height = 80
+z_0 = 0.3
+
+# turbines and farm
+bounds = np.array([[0, 4000], [0, 3500]])
+
+# windspeed_array, theta_array, wind_prob = read_windrose()
+windspeed_array = np.array([12])
+theta_array = np.array([0])
+wind_prob = np.array([1])
+
+
+N = 26
+n_pop = 200
+elit_frac = 0.05
+cross_frac = 0.8
+tour_size = 4
+mutat_frac = 0.01
+
 
 def init_random(pop, n_pop, bounds, N):
     for i in range(n_pop):
@@ -89,98 +110,43 @@ def mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_genes, b_range):
 
     new_pop[n_pop - mutat_num:n_pop, 1:] = np.reshape(gene_pool, (mutat_num, n_var))
 
-# turbine data
-diameter = 82
-height = 80
-z_0 = 0.3
 
-# turbines and farm
-bounds = np.array([[0, 4000], [0, 3500]])
 b_range = np.array([bounds[0, 1] - bounds[0, 0], bounds[1, 1] - bounds[1, 0]])
-# windspeed_array, theta_array, wind_prob = read_windrose()
-windspeed_array = np.array([12])
-theta_array = np.array([0])
-wind_prob = np.array([1])
+n_var = 2 * N
+elit_num = int(elit_frac*n_pop)
+cross_num = int(cross_frac * (n_pop - elit_num))
+mutat_num = n_pop - elit_num - cross_num
+mutat_genes = int(mutat_frac*mutat_num*n_var)
+old_pop = np.zeros((n_pop, n_var + 1))
+new_pop = np.zeros((n_pop, n_var + 1))
 
-# Ns = np.linspace(30, 90, 13)
-Ns = [80]
+generations = 100*N
 
-for N in Ns:
-    N = int(N)
-    # N = 26
-    n_var = 2 * N
-    # optimizer variables
-    n_pop = 200
-    elit_num = int(0.05*n_pop)
-    cross_frac = 0.8
-    cross_num = int(cross_frac * (n_pop - elit_num))
-    tour_size = 4
-    mutat_gene = 1
-    range_par = 0.5
-    mutat_num = n_pop - elit_num - cross_num
-    old_pop = np.zeros((n_pop, n_var + 1))
-    new_pop = np.zeros((n_pop, n_var + 1))
+# start algorithm
+init_random(old_pop, n_pop, bounds, N)
+old_pop = init_fitness(
+    old_pop,
+    n_pop,
+    bounds,
+    diameter,
+    height,
+    z_0,
+    windspeed_array,
+    theta_array,
+    wind_prob,
+)
 
-    generations = 100*N
+a = time()
 
-    # start algorithm
-    init_random(old_pop, n_pop, bounds, N)
-    old_pop = init_fitness(
-        old_pop,
-        n_pop,
-        bounds,
-        diameter,
-        height,
-        z_0,
-        windspeed_array,
-        theta_array,
-        wind_prob,
-    )
-
-    a = time()
-
-    try:
-        for gen in bar(range(generations)):
-            elite(new_pop, old_pop, elit_num)
-            cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size)
-            mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_gene, b_range, range_par)
-            new_pop = fitness(
-                new_pop,
-                n_pop,
-                elit_num,
-                bounds,
-                diameter,
-                height,
-                z_0,
-                windspeed_array,
-                theta_array,
-                wind_prob,
-            )
-            # print("{} - {}, {}, {}".format(gen, new_pop[0, 0], new_pop[1, 0], new_pop[2, 0]))
-            old_pop = new_pop
-
-    except KeyboardInterrupt:
-        print("Getting the values from last population...\n")
-
-    b = time()
-
-    algo_data = [
-            "Genetic",
-            "n_pop: {}\nelit_num: {}\ncross: {}\nmutate_gene: {}\nrange_par: {}\ngen: {}".format(
-            n_pop, elit_num, cross_frac, mutat_gene, range_par, generations
-            ),
-            # "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: {}\ntime: {:.3f}s".format(
-            # N, diameter, height, 'tejas', new_pop[0, 0], b-a
-            "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: ${:.3f}M\ntime: {:.3f}s".format(
-            N, diameter, height, 'tejas', -new_pop[0, 0]/1e6, b-a
-            ),
-            "genetic/N_{}".format(N),
-    ]
-    # print("Profit - {}".format(new_pop[0, 0]))
-    print("Profit - ${:.3f}M".format(-new_pop[0, 0]/1e6))
-    get_wake_plots(
-            new_pop[0, 1::2],
-            new_pop[0, 2::2],
+try:
+    for gen in bar(range(generations)):
+        elite(new_pop, old_pop, elit_num)
+        cross(new_pop, old_pop, n_pop, n_var, elit_num, cross_num, tour_size)
+        mutate(new_pop, old_pop, n_pop, mutat_num, n_var, mutat_genes, b_range)
+        new_pop = fitness(
+            new_pop,
+            n_pop,
+            elit_num,
             bounds,
             diameter,
             height,
@@ -188,5 +154,36 @@ for N in Ns:
             windspeed_array,
             theta_array,
             wind_prob,
-            algo_data
-    )
+        )
+        # print("{} - {}, {}, {}".format(gen, new_pop[0, 0], new_pop[1, 0], new_pop[2, 0]))
+        old_pop = new_pop
+
+except KeyboardInterrupt:
+    print("Getting the values from last population...\n")
+
+b = time()
+
+algo_data = [
+        "Genetic",
+        "n_pop: {}\nelit_frac {}\ncross: {}\ntour_size: {}\nmutat_frac: {}\ngen: {}".format(
+        n_pop, elit_frac, cross_frac, tour_size, mutat_frac, generations
+        ),
+        "n_turb: {}\ndiameter: {}\nheight: {}\ncost_model: {}\nprofit: ${:.3f}M\ntime: {:.3f}s".format(
+        N, diameter, height, 'tejas', -new_pop[0, 0]/1e6, b-a
+        ),
+        "genetic/N_{}".format(N),
+]
+# print("Profit - {}".format(new_pop[0, 0]))
+print("Profit - ${:.3f}M".format(-new_pop[0, 0]/1e6))
+get_wake_plots(
+        new_pop[0, 1::2],
+        new_pop[0, 2::2],
+        bounds,
+        diameter,
+        height,
+        z_0,
+        windspeed_array,
+        theta_array,
+        wind_prob,
+        algo_data
+)
